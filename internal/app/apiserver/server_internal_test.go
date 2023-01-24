872edf30d2e6bcec834fd1e82b9de0af830909a1
Arsenii <origingoy@gmail.com>
2023-01-24 21:41:48 +0200

    added tests for sign up and sign in

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -1,19 +1,111 @@
 package apiserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/arsu4ka/go_rest_api/internal/app/model"
 	"github.com/arsu4ka/go_rest_api/internal/app/store/teststore"
+	"github.com/gorilla/sessions"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestServer_HandleUsersCreate(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	server := NewServer(teststore.New(), sessions.NewCookieStore([]byte("secret")))
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    "user3@gmail.com",
+				"password": "user3pass",
+			},
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "data",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid password and email",
+			payload: map[string]string{
+				"email":    "cool",
+				"password": "hi user",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+	}
 
-	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
-	assert.Equal(t, rec.Code, http.StatusOK)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			data := &bytes.Buffer{}
+			json.NewEncoder(data).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/users", data)
+			server.ServeHTTP(rec, req)
+			assert.Equal(t, rec.Code, tc.expectedCode)
+		})
+	}
+
+}
+
+func TestServer_HandleSesssionsCreate(t *testing.T) {
+	userTest := model.TestUser(t)
+	server := NewServer(teststore.New(), sessions.NewCookieStore([]byte("secret")))
+	server.store.User().Create(userTest)
+
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    userTest.Email,
+				"password": userTest.Password,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid email",
+			payload: map[string]string{
+				"email":    "invalid",
+				"password": userTest.Password,
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+		{
+			name: "invalid password",
+			payload: map[string]string{
+				"email":    userTest.Email,
+				"password": "invalid",
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			data := &bytes.Buffer{}
+			json.NewEncoder(data).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/sessions", data)
+			server.ServeHTTP(rec, req)
+			assert.Equal(t, rec.Code, tc.expectedCode)
+		})
+	}
+
 }
