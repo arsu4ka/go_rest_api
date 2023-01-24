edce9fa0887e8e4e602714102fa7342e730839e0
Arsenii <origingoy@gmail.com>
2023-01-24 21:38:53 +0200

    main file to start server with mock db (not finished)

---
+++ cmd/testserver/main.go
@@ -0,0 +1,12 @@
+package main
+
+// import (
+// 	"github.com/arsu4ka/go_rest_api/internal/app/apiserver"
+// 	"github.com/arsu4ka/go_rest_api/internal/app/store/teststore"
+// )
+
+// func main() {
+// 	s := apiserver.NewServer(teststore.New())
+
+// 	s.ServeHTTP()
+// }
