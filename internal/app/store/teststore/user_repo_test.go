f61b232eeb565b3dcc7ed531121649ae5cdc366e
Arsenii <origingoy@gmail.com>
2023-01-26 14:36:30 +0200

    added test for findByID method of teststore

--- internal/app/store/teststore/user_repo_test.go
+++ internal/app/store/teststore/user_repo_test.go
@@ -23,9 +23,21 @@ func TestUserRepo_FindByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(userTest.Email)
 	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
 
-	s.User().Create(userTest)
+	err = s.User().Create(userTest)
+	assert.NoError(t, err)
 	u, err := s.User().FindByEmail(userTest.Email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 	assert.Equal(t, userTest.Email, u.Email)
+}
+
+func TestUserRepo_FindByID(t *testing.T) {
+	s := teststore.New()
+	userTest := model.TestUser(t)
+	err := s.User().Create(userTest)
+	assert.NoError(t, err)
+	u, err := s.User().FindByID(userTest.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+	assert.Equal(t, userTest.ID, u.ID)
 }
